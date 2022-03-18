8544169d809519e5fa9b5d0028981398c864eba6
aoirint <aoirint@gmail.com>
2022-03-18 10:05:35 +0900

    add clock

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,45 @@ import React from 'react';
 import './App.css';
 import { useGetSensorValueQuery } from './graphql-types';
 
+import dayjs from 'dayjs';
+import dayjsUtc from 'dayjs/plugin/utc';
+import dayjsTimeZone from 'dayjs/plugin/timezone';
+dayjs.extend(dayjsUtc);
+dayjs.extend(dayjsTimeZone);
+
 function App() {
+  const [currentTimeString, setCurrentTimeString] = React.useState<string>()
+  const currentTime = dayjs(currentTimeString)
+
   const {
     data: sensorValueData
   } = useGetSensorValueQuery({
-    fetchPolicy: 'cache-and-network',
+    fetchPolicy: 'no-cache',
     pollInterval: 1000 // ms
   })
 
   const light = sensorValueData?.light?.[0]?.value
-  const timestamp = sensorValueData?.light?.[0]?.timestamp
+  const timestampString = sensorValueData?.light?.[0]?.timestamp
+  const timestamp = timestampString ? dayjs(timestampString).tz('Asia/Tokyo') : undefined
   const humidity = sensorValueData?.humidity?.[0]?.value
   const temperature = sensorValueData?.temperature?.[0]?.value
   const mhz19Co2 = sensorValueData?.mhz19Co2?.[0]?.value
 
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      const nextCurrentTimeString = dayjs().tz('Asia/Tokyo').format()
+      setCurrentTimeString(nextCurrentTimeString)
+    })
+
+    return () => {
+      clearInterval(interval)
+    }
+  })
+
   return (
     <div className="App">
       <p>
-        Timestamp: {timestamp}
+        {currentTime?.format('M/D HH:mm:ss')}
       </p>
       <p>
         Light: {light ? (light > 150 ? 'On' : 'Off') : ''}
