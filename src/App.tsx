b61892d12fb52789bd1948de11cf1a7c1b672f96
aoirint <aoirint@gmail.com>
2022-03-18 09:48:19 +0900

    add Apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useGetSensorValueQuery } from './graphql-types';
 
 function App() {
+  const {
+    data: sensorValueData
+  } = useGetSensorValueQuery({
+    fetchPolicy: 'cache-and-network',
+    pollInterval: 1000 // ms
+  })
+
+  const light = sensorValueData?.light?.[0]?.value ?? 0
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Light: {light > 150 ? 'On' : 'Off'}
+      </p>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider
+} from "@apollo/client";
+import assert from 'assert';
+
+const API_URL = process.env.REACT_APP_API_URL
+const API_AUTHORIZATION = process.env.REACT_APP_API_AUTHORIZATION
+
+assert(API_URL)
+assert(API_AUTHORIZATION)
+
+const client = new ApolloClient({
+  uri: API_URL,
+  cache: new InMemoryCache(),
+  headers: {
+    authorization: API_AUTHORIZATION
+  }
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
